96724a5d3b967c53c7fc0ce402c9423232e9f05f
nothingx-x <sirmrmmr@gmail.com>
2025-11-10 10:03:20 +0330

    Add next-sitemap package and generate sitemap.xml

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+	siteUrl: "https://mongocraft.vercel.app",
+	generateRobotsTxt: true,
+	sitemapSize: 5000,
+	changefreq: "weekly",
+	priority: 1.0
+};
